3cecccac26b7a54f24904c36f47aafa9bbd0b90d
udbhav1 <umuthakana@gmail.com>
2022-01-11 12:49:13 -0500

    rename macro

--- programs/auctionhouse/src/lib.rs
+++ programs/auctionhouse/src/lib.rs
@@ -18,14 +18,13 @@ pub mod auctionhouse {
         let auction = &mut ctx.accounts.auction;
         let owner: &Signer = &ctx.accounts.owner;
         let clock: Clock = Clock::get().unwrap();
-
         let cur_time: u64 = clock.unix_timestamp as u64;
 
-        check!(title.chars().count() <= 50, Err(AuctionError::TitleOverflow.into()));
-        check!(increment != 0, Err(AuctionError::InvalidIncrement.into()));
-        check!(start_time < end_time, Err(AuctionError::InvalidStartTime.into()));
-        check!(cur_time > start_time || start_time == 0, Err(AuctionError::InvalidStartTime.into()));
-        check!(cur_time < end_time, Err(AuctionError::InvalidEndTime.into()));
+        require!(title.chars().count() <= 50, Err(AuctionError::TitleOverflow.into()));
+        require!(increment != 0, Err(AuctionError::InvalidIncrement.into()));
+        require!(start_time < end_time, Err(AuctionError::InvalidStartTime.into()));
+        require!(cur_time > start_time || start_time == 0, Err(AuctionError::InvalidStartTime.into()));
+        require!(cur_time < end_time, Err(AuctionError::InvalidEndTime.into()));
 
         auction.owner = *owner.key;
         auction.start_time = if start_time == 0 { cur_time } else { start_time };
@@ -59,10 +58,10 @@ pub mod auctionhouse {
         let clock: Clock = Clock::get().unwrap();
         let cur_time: u64 = clock.unix_timestamp as u64;
 
-        check!(!auction.cancelled, Err(AuctionError::BidAfterCancelled.into()));
-        check!(cur_time > auction.start_time, Err(AuctionError::BidBeforeStart.into()));
-        check!(cur_time < auction.end_time, Err(AuctionError::BidAfterClose.into()));
-        check!(*bidder.key != auction.owner, Err(AuctionError::OwnerCannotBid.into()));
+        require!(!auction.cancelled, Err(AuctionError::BidAfterCancelled.into()));
+        require!(cur_time > auction.start_time, Err(AuctionError::BidBeforeStart.into()));
+        require!(cur_time < auction.end_time, Err(AuctionError::BidAfterClose.into()));
+        require!(*bidder.key != auction.owner, Err(AuctionError::OwnerCannotBid.into()));
 
         let index = auction.bidders.iter().position(|&x| x == *bidder.key);
 
@@ -71,14 +70,14 @@ pub mod auctionhouse {
         let mut new_bidder = false;
 
         if let None = index {
-            check!(auction.bidders.len() < (auction.bidder_cap as usize), Err(AuctionError::BidderCapReached.into()));
+            require!(auction.bidders.len() < (auction.bidder_cap as usize), Err(AuctionError::BidderCapReached.into()));
             new_bidder = true;
         } else {
             total_bid += auction.bids[index.unwrap()];
         }
 
-        check!(total_bid > auction.bid_floor, Err(AuctionError::UnderBidFloor.into()));
-        check!(total_bid > (auction.highest_bid + auction.min_bid_increment), Err(AuctionError::InsufficientBid.into()));
+        require!(total_bid > auction.bid_floor, Err(AuctionError::UnderBidFloor.into()));
+        require!(total_bid > (auction.highest_bid + auction.min_bid_increment), Err(AuctionError::InsufficientBid.into()));
 
         if new_bidder {
             auction.bidders.push(*bidder.key);
@@ -108,7 +107,7 @@ pub mod auctionhouse {
         let clock: Clock = Clock::get().unwrap();
         let cur_time: u64 = clock.unix_timestamp as u64;
 
-        check!(auction.cancelled || cur_time > auction.end_time, Err(AuctionError::AuctionNotOver.into()));
+        require!(auction.cancelled || cur_time > auction.end_time, Err(AuctionError::AuctionNotOver.into()));
 
         let index = auction.bidders.iter().position(|&x| x == *bidder.key);
         if let None = index {
@@ -137,7 +136,7 @@ pub mod auctionhouse {
         let clock: Clock = Clock::get().unwrap();
         let cur_time: u64 = clock.unix_timestamp as u64;
 
-        check!(auction.cancelled || cur_time > auction.end_time, Err(AuctionError::AuctionNotOver.into()));
+        require!(auction.cancelled || cur_time > auction.end_time, Err(AuctionError::AuctionNotOver.into()));
 
         let index = auction.bidders.iter().position(|&x| x == auction.highest_bidder);
         if let None = index {
